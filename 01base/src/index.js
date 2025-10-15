79fbdfe0215df1f78d91af8d707276b103cabd2e
Carlos-Godoi <168770521+Carlos-Godoi@users.noreply.github.com>
2025-10-15 15:20:05 -0300

    Aula 04 - Render
    Exclusão de todos os arquivos da pasta SRC, criados automaticamente na criação do projeto.
    
    Criação de um novo arquivo (index.js).

--- 01base/src/index.js
+++ 01base/src/index.js
@@ -1,17 +1,19 @@
+// Importar módulos
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// 1. Importar 'createRoot' de 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+
+// 2. Criar a raiz (root)
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3. Renderizar com a root
+root.render(
+    
+    // O <React.StrictMode> é opcional, mas recomendado para detectar problemas.
+    <React.StrictMode>
+        <h1>Meu primeiro render</h1>
+    </React.StrictMode>
+);
