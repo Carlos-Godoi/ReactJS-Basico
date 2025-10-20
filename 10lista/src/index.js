dcd8323725698519bd1056d0f376ae63500353f2
Carlos-Godoi <168770521+Carlos-Godoi@users.noreply.github.com>
2025-10-20 16:22:40 -0300

    Aula 17 - CSS
    Aplicar estilo CSS de três maneiras:
    inline;
    variáveis/constantes;
    arquivo estilo.css;

--- 10lista/src/index.js
+++ 10lista/src/index.js
@@ -4,6 +4,9 @@ import React from 'react';
 // Importar ReactRoot de 'react-dom/cliente'
 import ReactDOM from 'react-dom/client';
 
+// Importar arquivo CSS
+import './estilos.css';
+
 // Componente
 class Componente extends React.Component {
 
@@ -17,12 +20,22 @@ class Componente extends React.Component {
             return <li key={index}>{index} - {cor}</li>
         });
 
-
+        // Estilo
+        const estilo = {
+            color : 'blue',
+            borderBottom: 'solid 5px green'
+        }
 
         // Retorno
         return (
+            
             <ul>
                 {listar}
+
+                <h1 style={{color:'red', backgroundColor:'yellow'}}>CSS interno (inline)</h1> 
+
+                <h1 style={estilo}>CSS através de variáveis e constantes</h1>
+
             </ul>
         );
 
