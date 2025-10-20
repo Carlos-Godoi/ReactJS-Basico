cde98b0f11d5bbec2d068dec5903c5648d30b40d
Carlos-Godoi <168770521+Carlos-Godoi@users.noreply.github.com>
2025-10-20 18:24:46 -0300

    Aula 24 - Manipulação de elementos de formulário
    Implementação de múltiplos campos no formulário e criando uma única função para receber o STATES ( nome, idade ).

--- 14campos-multiplos/src/index.js
+++ 14campos-multiplos/src/index.js
@@ -18,13 +18,8 @@ class Componente extends React.Component{
     }
 
     // Função - Nome
-    funcaoNome = (campo) => {
-        this.setState({nome : campo.target.value});
-    }
-
-    // Função - Idade
-    funcaoIdade = (campo) => {
-        this.setState({idade : campo.target.value});
+    enviarParaState = (campo) => {
+        this.setState({[campo.target.name] : campo.target.value});
     }
 
     // Render
@@ -33,8 +28,8 @@ class Componente extends React.Component{
         // Retornar
         return(
             <form>
-                <input type='text' placeholder='Nome' onChange={this.funcaoNome}/>
-                <input type='number' placeholder='Idade' onChange={this.funcaoIdade}/> 
+                <input type='text' placeholder='Nome' name='nome' onChange={this.enviarParaState}/>
+                <input type='number' placeholder='Idade' name='idade' onChange={this.enviarParaState}/> 
 
                 <p>{this.state.nome}</p>
                 <p>{this.state.idade}</p>
